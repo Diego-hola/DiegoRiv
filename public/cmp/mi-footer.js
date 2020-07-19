cd6f334c04de49fb073be5bc908309495a5563c2
Diego-hola <riversdiego69@gmail.com>
2020-07-19 18:44:17 -0500

    r

--- public/cmp/mi-footer.js
+++ public/cmp/mi-footer.js
@@ -2,6 +2,12 @@ class MiFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /* html */
             `Copyright &copy; 2020 Rivera Gonzalez Diego.`;
+            `<footer style=" background-color: orange; padding: 4rem; margin-top: 3rem; text-align: center; font-size: 1rem; width: 100%; bottom: 0;">Copyright &copy; 2020 Rivera Gonzalez Diego .</footer>`;
+            
+        }
+    }
+    customElements.define("mi-footer", MiFooter);
+    
     }
 }
 customElements.define("mi-footer", MiFooter);
